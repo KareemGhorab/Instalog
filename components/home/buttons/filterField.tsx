db75dab2b3b0745114e52e9050833a4dcfcb8691
Kareem Ghorab <kareemghorab3@gmail.com>
2023-01-11 19:42:22 +0200

    Fixed filter bug

--- components/home/buttons/filterField.tsx
+++ components/home/buttons/filterField.tsx
@@ -5,30 +5,32 @@ export default function FilterField(props: {
 	onReset: any
 	onFilter: (id: string) => void
 	documents: User[]
-    title: string
+	title: string
 }) {
 	const { onFilter, onReset, documents, title } = props
 
 	return (
-		<fieldset className="border-b border-primary-500 mb-5">
-			<h3>{title}</h3>
-			<li
-				className="text-primary-500 cursor-pointer mt-3
-							hover:text-primary-700"
-				onClick={onReset}
-			>
-				all
-			</li>
-			{documents.map((user: User) => (
+		<>
+			<fieldset className="border-b border-primary-500 mb-5">
+				<h3>{title}</h3>
 				<li
 					className="text-primary-500 cursor-pointer mt-3
-							hover:text-primary-700"
-					key={user.id}
-					onClick={() => onFilter(user.id)}
+				hover:text-primary-700"
+					onClick={onReset}
 				>
-					{user.email}
+					all
 				</li>
-			))}
-		</fieldset>
+				{documents.length? documents.map((user: User) => (
+					<li
+						className="text-primary-500 cursor-pointer mt-3
+				hover:text-primary-700"
+						key={user.id}
+						onClick={() => onFilter(user.id)}
+					>
+						{user.email}
+					</li>
+				)) : "Loading"}
+			</fieldset>
+		</>
 	)
 }
